891b09949a5c421c27840fb58aa5f47b7ba147a2
Robert Karlsson <robert.karlsson@zocom.se>
2014-12-15 09:54:51 +0100

    sahn13 måndag

---
+++ distribuerade/vecka2/labb/Telefonbok/Telefonbok/Models/Kontakt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Telefonbok.Models
+{
+    public class Kontakt
+    {
+        public int Id { get; set; }
+
+        /* Till del 2 */
+
+        /*public XElement ToXml()
+        {
+            var newXML = new XElement("Kontakt",
+                new XElement("Id", Id),
+                new XElement("Förnamn", Förnamn),
+                new XElement("Efternamn", Efternamn),
+                new XElement("Telefonnummer", Telefonnummer));
+
+            return newXML;
+        }
+        
+        public static Kontakt FromXml(XElement xml)
+        {
+            Kontakt nyKontakt = new Kontakt();
+            nyKontakt.Id = (int)xml.Element("Id");
+            nyKontakt.Förnamn = xml.Element("Förnamn").Value;
+            nyKontakt.Efternamn = xml.Element("Efternamn").Value;
+            nyKontakt.Telefonnummer = xml.Element("Telefonnummer").Value;
+
+            return nyKontakt;
+        }*/
+    }
+}
---
+++ distribuerade/vecka2/labb/Telefonbok_Karlsson_Robert/Telefonbok/Models/Kontakt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Telefonbok.Models
+{
+    public class Kontakt
+    {
+        public int Id { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+        public string Telefonnummer { get; set; }
+        
+        public XElement ToXml()
+        {
+        var newXML = new XElement("Kontakt", 
+            new XElement("Id", Id),
+            new XElement("Förnamn", Förnamn),
+            new XElement("Efternamn", Efternamn),
+            new XElement("Telefonnummer", Telefonnummer));
+        
+        return newXML;
+        }
+
+        public static Kontakt FromXml(XElement xml)
+        {
+            Kontakt nyKontakt = new Kontakt();
+            nyKontakt.Id = (int)xml.Element("Id");
+            nyKontakt.Förnamn = xml.Element("Förnamn").Value;
+            nyKontakt.Efternamn = xml.Element("Efternamn").Value;
+            nyKontakt.Telefonnummer = xml.Element("Telefonnummer").Value;
+
+        return nyKontakt;
+        }
+        
+    }
+}
